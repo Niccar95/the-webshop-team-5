e11295bf83b195bc8aa190763da9425fdaa84f9a
Niccar95 <niccar.95@outlook.com>
2023-12-26 11:37:45 +0100

    påbörjad projekt :tada:

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+// vite.config.ts
+import checker from 'vite-plugin-checker'
+export default {
+  plugins: [
+    checker({
+      // e.g. use TypeScript check
+      typescript: true,
+    }),
+  ],
+}
