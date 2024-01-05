0188c881276f434f44f0798c52c4e93463dd8814
Niccar95 <niccar.95@outlook.com>
2024-01-05 13:14:34 +0100

    nav bar hover färg tillagd i checkout

--- src/ts/checkout.ts
+++ src/ts/checkout.ts
@@ -8,7 +8,7 @@ console.log("checkout time :");
 
 let totalPrice:number = 0;
 
-const checkoutContainer = document.querySelector(".checkoutContainer");
+const checkoutContainer = document.querySelector(".checkoutContainer") as HTMLElement;
 const totalPriceP = document.querySelector(".totalPrice") as HTMLElement;
 const checkoutItems = document.querySelector(".checkoutItems") as HTMLElement;
 
@@ -31,83 +31,85 @@ function calculatePrice(product:Product){
 for (let i = 0; i < cart.length; i++) {
     
     function createCheckout(){    
-    const title = document.createElement("h4");
-    const image = document.createElement("img");
-    image.className = "productImage";
-    const price = document.createElement("p");
-
-    //vi ska också kunna se antal varor i korgen också!!!
-
-    title.innerHTML = cart[i].productTitle + " X " + cart[i].productAmount;
-    image.src = cart[i].productImageURL;
-    price.innerHTML = cart[i].productPrice.toString() + "€";
-
-    checkoutItems.appendChild(title);
-    checkoutItems.appendChild(image);
-    checkoutItems.appendChild(price);
-    calculatePrice(cart[i]);
-
-
-    const addButton = document.createElement("button");
-    const removeButton = document.createElement("button");
-    const checkoutButtonContainer = document.createElement("section");
-
-    addButton.className = "checkoutButton";
-    removeButton.className = "checkoutButton";
-    checkoutButtonContainer.className = "checkoutButtonContainer";
-
-  
-    addButton.innerHTML = "+";
-    removeButton.innerHTML = "-";
-
-    checkoutItems?.appendChild(checkoutButtonContainer);
-    checkoutButtonContainer?.appendChild(addButton);
-    checkoutButtonContainer?.appendChild(removeButton);
-
-    checkoutContainer?.appendChild(checkoutItems);
+      if(cart[i].productAmount > 0){
+        const title = document.createElement("h4");
+        const image = document.createElement("img");
+        image.className = "productImage";
+        const price = document.createElement("p");
     
-    updateTotalPrice();
-
-
-    const updateQuantity = () => {
+        //vi ska också kunna se antal varor i korgen också!!!
+    
         title.innerHTML = cart[i].productTitle + " X " + cart[i].productAmount;
-      };
-
-      updateQuantity();
-
-      addButton.addEventListener("click", () => {
-        const index = cart.indexOf(cart[i]);
-        cart[index].productAmount++;
-        localStorage.setItem("userCart", JSON.stringify(cart));
-        updateQuantity();
-        calculatePrice(cart[i]);
-        updateTotalPrice();
-        console.log(totalPrice);
-      });
+        image.src = cart[i].productImageURL;
+        price.innerHTML = cart[i].productPrice.toString() + "€";
     
-      removeButton.addEventListener("click", () => {
-        const handleRemoveButtonClick = (currentProduct: Product) => {
-          const indexToRemove = cart.indexOf(cart[i]);
-          //cart.splice(indexToRemove, 1);
-     
-          if (currentProduct.productAmount > 0) {
-            currentProduct.productAmount -= 1;
-            updateQuantity();
-
-     
-            if (currentProduct.productAmount === 0) {
-              cart.splice(indexToRemove, 1);
-              if (checkoutItems != null) {
-                checkoutItems.remove();
-              }
-            }
+        checkoutItems.appendChild(title);
+        checkoutItems.appendChild(image);
+        checkoutItems.appendChild(price);
+        calculatePrice(cart[i]);
+    
+    
+        const addButton = document.createElement("button");
+        const removeButton = document.createElement("button");
+        const checkoutButtonContainer = document.createElement("section");
+    
+        addButton.className = "checkoutButton";
+        removeButton.className = "checkoutButton";
+        checkoutButtonContainer.className = "checkoutButtonContainer";
+    
+      
+        addButton.innerHTML = "+";
+        removeButton.innerHTML = "-";
+    
+        checkoutItems?.appendChild(checkoutButtonContainer);
+        checkoutButtonContainer?.appendChild(addButton);
+        checkoutButtonContainer?.appendChild(removeButton);
+    
+        checkoutContainer?.appendChild(checkoutItems);
+        
+        updateTotalPrice();
+    
+    
+        const updateQuantity = () => {
+            title.innerHTML = cart[i].productTitle + " X " + cart[i].productAmount;
+          };
+    
+          updateQuantity();
+    
+          addButton.addEventListener("click", () => {
+            const index = cart.indexOf(cart[i]);
+            cart[index].productAmount++;
             localStorage.setItem("userCart", JSON.stringify(cart));
-          }
-     
-          //displayAmount(product.productAmount);
-        };
-        handleRemoveButtonClick(cart[i]);
-      });
+            updateQuantity();
+            calculatePrice(cart[i]);
+            updateTotalPrice();
+            console.log(totalPrice);
+          });
+        
+          removeButton.addEventListener("click", () => {
+            const handleRemoveButtonClick = () => {
+              const index = cart.indexOf(cart[i]);
+              if (cart[index].productAmount > 0) {
+                cart[index].productAmount--;
+                updateQuantity();
+                localStorage.setItem("userCart", JSON.stringify(cart));
+                if (cart[index].productAmount === 0) {
+                    cart.splice(index, i);
+
+                    //checkoutItems.remove();
+                    checkoutContainer.replaceChildren();
+                    createCheckout();
+                    localStorage.setItem("userCart", JSON.stringify(cart));
+                }
+               
+              }
+         
+              //displayAmount(product.productAmount);
+            };
+            handleRemoveButtonClick();
+          });
+      }
+   
     }
     createCheckout();
 }
