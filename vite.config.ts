65864d9c7c31671ccec3ea4165b697e526698082
Niccar95 <niccar.95@outlook.com>
2024-03-02 12:27:55 +0100

    deloyed website

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 // vite.config.ts
-import checker from 'vite-plugin-checker'
+import checker from "vite-plugin-checker";
 export default {
+  base: "/the-webshop-team-5/",
   plugins: [
     checker({
       // e.g. use TypeScript check
       typescript: true,
     }),
   ],
-}
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        projects: "checkout.html",
+      },
+    },
+  },
+};
